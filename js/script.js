47491b782c1eb179a8467167344cb34753f4e363
claudio <claudio.cenghialta@gmail.com>
2020-09-02 12:17:35 +0200

    milestone 1 completata

--- js/script.js
+++ js/script.js
@@ -28,34 +28,73 @@ https://api.themoviedb.org/3/search/movie
 
 
 $(document).ready(function () {
-    var datiRicerca = "";
+    $('#cerca-input').val("");
+    $('#cerca-btn').click(iniziaRicerca)
+    $('#cerca-input').keyup(function () {
+        if (event.which == 13 || event.keyCode == 13) {
+            iniziaRicerca();
+        }
+    })
+
+}); //fine document ready
+
+function iniziaRicerca() {
+    //prendo il valore inserito nell'input e lo salvo in una variabile
+    var datiRicerca = $('#cerca-input').val();
+    //controllo valore stringa 
+    if (datiRicerca != "") {
+        // svuoto l'elenco dei film già cercati in precedenza
+        $('.film-dettaglio').empty();
+        //lancio funzione per ricerca film
+        cercaFilm(datiRicerca);
+    } else {
+        // svuoto l'elenco dei film già cercati in precedenza
+        $('.film-dettaglio').empty();
+        //stampo messaggio di errore al posto dell'elenco titoli
+        $('.film-dettaglio').html('Attenzione, la ricerca non può essere vuota');
+    }
+}
+
+function cercaFilm(stringaRicerca) {
+    //faccio chiamata ajax sulla base del valore salvato nella variabile passata
     $.ajax({
         url: "https://api.themoviedb.org/3/search/movie",
         method: "GET",
         data: {
             api_key: "f55f5e2e7cdc1cc61c195d269b630b9c",
             language: "it-IT",
-            query: datiRicerca,
+            query: stringaRicerca, //variabile passata 
             include_adult: "false"
         },
         success: function (resp) {
-            console.log(resp);
+            //elementi per template Handlebars
+            var source = document.getElementById("entry-template").innerHTML;
+            var template = Handlebars.compile(source);
+            //controllo che i risultati non siano zero, quindi non esistono film con quella stringa di ricerca
+            if (resp.results.length == 0) {
+                $('.film-dettaglio').html('Attenzione, la ricerca non ha prodotto alcun risultato');
+            } else {
+                //ciclo tutti i risultati ottenuti
+                for (var i = 0; i < resp.results.length; i++) {
+                    var context = {
+                        //prendo solo i valori da stampare a video e non tutti i dati ricevuti in risposta alla chiamata
+                        title: resp.results[i].title,
+                        original_title: resp.results[i].original_title,
+                        original_language: resp.results[i].original_language,
+                        vote_average: resp.results[i].vote_average
+                    };
+                    // stampo sui template Handlebars ogni risultato del ciclo for
+                    var html = template(context);
+                    $('.film-dettaglio').append(html)
+
+                }
+            }
         },
         error: function (resp) {
+            //segnalo errore e compilo log
             alert('errore')
             console.log(resp);
 
         },
-    })
-
-    // var source = document.getElementById("entry-template").innerHTML;
-    // var template = Handlebars.compile(source);
-
-    // var context = { title: "My New Post", body: "This is my first post!" };
-    // var html = template(context);
-
-
-
-
-
-});
+    }) //fine chiamata ajax
+}
